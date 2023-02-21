7da7f0acbabaf037362f14274c174da50c9f8cd2
Praful Agarwal <88195840+prafulagarwal1@users.noreply.github.com>
2023-02-21 12:06:02 +0530

    Add files via upload

---
+++ user-service/src/main/java/com/dp/user/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.dp.user.controller;
+
+import com.dp.user.VO.ResponseTemplateVO;
+import com.dp.user.entity.User;
+import com.dp.user.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.lang.*;
+
+@RestController
+@RequestMapping("/users")
+@Slf4j
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/")
+    public User saveUser(@RequestBody User user) {
+        log.info("Inside saveUser of UserController");
+        return userService.saveUser(user);
+    }
+
+    private static final String ServiceA = "serviceA";
+
+    @GetMapping("/{id}")
+
+    public ResponseTemplateVO getUserWithDepartment(@PathVariable("id") Long userId) {
+        log.info("Inside getUserWithDepartment of UserController");
+        return userService.getUserWithDepartment(userId);
+    }
+
+}
